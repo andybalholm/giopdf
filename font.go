a195cc51dcf9b81233f7227d0555adb9cb016320
Andy Balholm <andy@balholm.com>
2022-02-15 15:00:42 -0800

    Support TrueType fonts with builtin encoding.

--- font.go
+++ font.go
@@ -2,6 +2,7 @@ package giopdf
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 
@@ -9,9 +10,8 @@ import (
 	"github.com/andybalholm/giopdf/pdf"
 	"github.com/benoitkugler/textlayout/fonts"
 	"github.com/benoitkugler/textlayout/fonts/simpleencodings"
+	"github.com/benoitkugler/textlayout/fonts/truetype"
 	"github.com/benoitkugler/textlayout/fonts/type1"
-	"golang.org/x/image/font"
-	"golang.org/x/image/font/sfnt"
 	"golang.org/x/image/math/fixed"
 )
 
@@ -46,70 +46,114 @@ func scalePoint(p fixed.Point26_6, ppem fixed.Int26_6) f32.Point {
 	return f32.Pt(float32(p.X)/float32(ppem), -float32(p.Y)/float32(ppem))
 }
 
-// SimpleFontFromSFNT converts a parsed SFNT (TrueType or OpenType) font to a
+// SimpleFontFromSFNT converts an SFNT (TrueType or OpenType) font to a
 // SimpleFont with the specified encoding.
-func SimpleFontFromSFNT(sf *sfnt.Font, encoding [256]rune) (*SimpleFont, error) {
-	buf := new(sfnt.Buffer)
-	ppem := fixed.I(int(sf.UnitsPerEm()))
+func SimpleFontFromSFNT(data []byte, encoding pdf.Value) (*SimpleFont, error) {
+	f, err := truetype.Parse(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+
+	ppem := f.Upem()
+	scale := 1 / float32(ppem)
+	fm := f32.Affine2D{}.Scale(f32.Pt(0, 0), f32.Pt(scale, scale))
+
+	var GIDEncoding [256]fonts.GID
+
+	if !encoding.IsNull() {
+		enc, err := getEncoding(encoding)
+		if err != nil {
+			return nil, err
+		}
+		for b, r := range enc.ByteToRune() {
+			gi, ok := f.NominalGlyph(r)
+			if ok {
+				GIDEncoding[b] = gi
+			}
+		}
+	} else {
+		// The encoding in the font dictionary was missing, so use the font's
+		// builtin encoding.
+		fp, err := truetype.NewFontParser(bytes.NewReader(data))
+		if err != nil {
+			return nil, err
+		}
+		ct, err := fp.CmapTable()
+		if err != nil {
+			return nil, err
+		}
+		cmap3_0 := ct.FindSubtable(truetype.CmapID{3, 0})
+		if cmap3_0 != nil {
+			// If the font contains a (3, 0) subtable, the range of character codes must be one
+			// of the following: 0x0000 - 0x00FF, 0xF000 - 0xF0FF, 0xF100 - 0xF1FF, or
+			// 0xF200 - 0xF2FF. Depending on the range of codes, each byte from the string is
+			// prepended with the high byte of the range, to form a two-byte character, which
+			// is used to select the associated glyph description from the subtable.
+			iter := cmap3_0.Iter()
+			for iter.Next() {
+				code, gid := iter.Char()
+				switch code & 0xFF00 {
+				case 0x0000, 0xF000, 0xF100, 0xF200:
+					b := code & 0xFF
+					if GIDEncoding[b] == 0 {
+						GIDEncoding[b] = gid
+					}
+				}
+			}
+		}
+		cmap1_0 := ct.FindSubtable(truetype.CmapID{1, 0})
+		if cmap1_0 != nil {
+			// Otherwise, if the font contains a (1, 0) subtable, single bytes from the string are
+			// used to look up the associated glyph descriptions from the subtable.
+			iter := cmap1_0.Iter()
+			for iter.Next() {
+				code, gid := iter.Char()
+				if code > 255 {
+					continue
+				}
+				if GIDEncoding[code] == 0 {
+					GIDEncoding[code] = gid
+				}
+			}
+		}
+	}
 
 	simple := new(SimpleFont)
 
-	for i, c := range encoding {
+	for i, gi := range GIDEncoding {
 		var g Glyph
-		gi, err := sf.GlyphIndex(buf, c)
-		if err != nil {
-			return nil, err
-		}
-		width, err := sf.GlyphAdvance(buf, gi, ppem, font.HintingNone)
-		if err != nil {
-			return nil, err
-		}
-		g.Width = float32(width) / float32(ppem)
+		g.Width = f.HorizontalAdvance(gi) * scale
 
-		segments, err := sf.LoadGlyph(buf, gi, ppem, nil)
-		if err != nil {
-			return nil, err
+		gd := f.GlyphData(gi, ppem, ppem)
+		switch gd := gd.(type) {
+		case fonts.GlyphOutline:
+			g.Outlines = glyphOutline(gd, fm)
+		case fonts.GlyphSVG:
+			g.Outlines = glyphOutline(gd.Outline, fm)
+		case fonts.GlyphBitmap:
+			return nil, errors.New("bitmap fonts not supported")
 		}
-		var p PathBuilder
-		for _, seg := range segments {
-			p0 := scalePoint(seg.Args[0], ppem)
-			p1 := scalePoint(seg.Args[1], ppem)
-			p2 := scalePoint(seg.Args[2], ppem)
-			switch seg.Op {
-			case sfnt.SegmentOpMoveTo:
-				p.ClosePath()
-				p.MoveTo(p0.X, p0.Y)
-			case sfnt.SegmentOpLineTo:
-				p.LineTo(p0.X, p0.Y)
-			case sfnt.SegmentOpQuadTo:
-				p.QuadraticCurveTo(p0.X, p0.Y, p1.X, p1.Y)
-			case sfnt.SegmentOpCubeTo:
-				p.CurveTo(p0.X, p0.Y, p1.X, p1.Y, p2.X, p2.Y)
-			}
-		}
-		p.ClosePath()
-		g.Outlines = p.Path
 		simple.Glyphs[i] = g
 	}
 
 	return simple, nil
 }
 
-func getEncoding(e pdf.Value) ([256]string, error) {
+func getEncoding(e pdf.Value) (simpleencodings.Encoding, error) {
 	switch e.Kind() {
 	case pdf.Null:
-		return [256]string{}, nil
+		return simpleencodings.Encoding{}, nil
 
 	case pdf.Name:
 		switch e.Name() {
 		case "MacRomanEncoding":
-			return [256]string(simpleencodings.MacRoman), nil
+			return simpleencodings.MacRoman, nil
 		case "MaxExpertEncoding":
-			return [256]string(simpleencodings.MacExpert), nil
+			return simpleencodings.MacExpert, nil
 		case "WinAnsiEncoding":
-			return [256]string(simpleencodings.WinAnsi), nil
+			return simpleencodings.WinAnsi, nil
 		default:
-			return [256]string{}, fmt.Errorf("unknown encoding: %v", e)
+			return simpleencodings.Encoding{}, fmt.Errorf("unknown encoding: %v", e)
 		}
 
 	case pdf.Dict:
@@ -132,13 +176,18 @@ func getEncoding(e pdf.Value) ([256]string, error) {
 		return enc, nil
 
 	default:
-		return [256]string{}, fmt.Errorf("invalid encoding: %v", e)
+		return simpleencodings.Encoding{}, fmt.Errorf("invalid encoding: %v", e)
 	}
 }
 
-// SimpleFontFromType1 converts a parsed Type 1 font to a SimpleFont, using the
+// SimpleFontFromType1 converts a Type 1 font to a SimpleFont, using the
 // encoding provided (normally taken from the font dictionary).
-func SimpleFontFromType1(f *type1.Font, encoding pdf.Value) (*SimpleFont, error) {
+func SimpleFontFromType1(data []byte, encoding pdf.Value) (*SimpleFont, error) {
+	f, err := type1.Parse(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+
 	nameToGID := map[string]fonts.GID{}
 	for i := fonts.GID(0); ; i++ {
 		name := f.GlyphName(i)
@@ -176,30 +225,35 @@ func SimpleFontFromType1(f *type1.Font, encoding pdf.Value) (*SimpleFont, error)
 		if gd == nil {
 			continue
 		}
-		outline := gd.(fonts.GlyphOutline)
-		var p PathBuilder
-		for _, seg := range outline.Segments {
-			p0 := fm.Transform(f32.Point(seg.Args[0]))
-			p1 := fm.Transform(f32.Point(seg.Args[1]))
-			p2 := fm.Transform(f32.Point(seg.Args[2]))
-			switch seg.Op {
-			case fonts.SegmentOpMoveTo:
-				p.ClosePath()
-				p.MoveTo(p0.X, p0.Y)
-			case fonts.SegmentOpLineTo:
-				p.LineTo(p0.X, p0.Y)
-			case fonts.SegmentOpQuadTo:
-				p.QuadraticCurveTo(p0.X, p0.Y, p1.X, p1.Y)
-			case fonts.SegmentOpCubeTo:
-				p.CurveTo(p0.X, p0.Y, p1.X, p1.Y, p2.X, p2.Y)
-			}
-		}
-		p.ClosePath()
-		g.Outlines = p.Path
+		g.Outlines = glyphOutline(gd.(fonts.GlyphOutline), fm)
 		simple.Glyphs[i] = g
 	}
 
 	return simple, nil
+}
+
+// glyphOutline converts outline to our path format, transforming the points
+// with fm.
+func glyphOutline(outline fonts.GlyphOutline, fm f32.Affine2D) []PathElement {
+	var p PathBuilder
+	for _, seg := range outline.Segments {
+		p0 := fm.Transform(f32.Point(seg.Args[0]))
+		p1 := fm.Transform(f32.Point(seg.Args[1]))
+		p2 := fm.Transform(f32.Point(seg.Args[2]))
+		switch seg.Op {
+		case fonts.SegmentOpMoveTo:
+			p.ClosePath()
+			p.MoveTo(p0.X, p0.Y)
+		case fonts.SegmentOpLineTo:
+			p.LineTo(p0.X, p0.Y)
+		case fonts.SegmentOpQuadTo:
+			p.QuadraticCurveTo(p0.X, p0.Y, p1.X, p1.Y)
+		case fonts.SegmentOpCubeTo:
+			p.CurveTo(p0.X, p0.Y, p1.X, p1.Y, p2.X, p2.Y)
+		}
+	}
+	p.ClosePath()
+	return p.Path
 }
 
 func importPDFFont(f pdf.Font) (Font, error) {
@@ -213,19 +267,7 @@ func importPDFFont(f pdf.Font) (Font, error) {
 		if err != nil {
 			return nil, err
 		}
-		sf, err := sfnt.Parse(data)
-		if err != nil {
-			return nil, err
-		}
-
-		switch f.V.Key("Encoding").Name() {
-		case "WinAnsiEncoding":
-			return SimpleFontFromSFNT(sf, pdf.WinAnsiEncoding)
-		case "MacRomanEncoding":
-			return SimpleFontFromSFNT(sf, pdf.MacRomanEncoding)
-		default:
-			return nil, fmt.Errorf("%v: unknown encoding: %v", f.V.Key("BaseFont"), f.V.Key("Encoding"))
-		}
+		return SimpleFontFromSFNT(data, f.V.Key("Encoding"))
 
 	case "Type1":
 		file := f.V.Key("FontDescriptor").Key("FontFile")
@@ -236,11 +278,7 @@ func importPDFFont(f pdf.Font) (Font, error) {
 		if err != nil {
 			return nil, err
 		}
-		t1f, err := type1.Parse(bytes.NewReader(data))
-		if err != nil {
-			return nil, err
-		}
-		return SimpleFontFromType1(t1f, f.V.Key("Encoding"))
+		return SimpleFontFromType1(data, f.V.Key("Encoding"))
 
 	default:
 		return nil, fmt.Errorf("%v is an unsupported font type (%v)", f.V.Key("BaseFont"), f.V.Key("Subtype"))
