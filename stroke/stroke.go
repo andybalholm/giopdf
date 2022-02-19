e811277cc8f602e1414ce990c5463038f82f4e1e
Andy Balholm <andy@balholm.com>
2022-02-19 12:12:40 -0800

    Add Stroke function (for a path with multiple contours).

--- stroke/stroke.go
+++ stroke/stroke.go
@@ -1,9 +1,9 @@
 package stroke
 
-// StrokeContour strokes a single contour (connected series of segments). If c
+// strokeContour strokes a single contour (connected series of segments). If c
 // is closed, it returns both the outer contour of the stroke and the inner
 // one (the outline of the hole in the middle). Otherwise inner is nil.
-func StrokeContour(c []Segment, width float32) (outer, inner []Segment) {
+func strokeContour(c []Segment, width float32) (outer, inner []Segment) {
 	halfWidth := width / 2
 	for _, s := range c {
 		right, left := OffsetCurves(s, halfWidth)
@@ -30,4 +30,19 @@ func StrokeContour(c []Segment, width float32) (outer, inner []Segment) {
 		outer = append(outer, LinearSegment(inner[0].Start, outer[0].Start))
 		return outer, nil
 	}
+}
+
+// Stroke returns outlines for the contours in path. Both in the parameter and
+// in the return value, each element of the slice is a contour (a connected
+// series of segments).
+func Stroke(path [][]Segment, width float32) [][]Segment {
+	var result [][]Segment
+	for _, c := range path {
+		outer, inner := strokeContour(c, width)
+		result = append(result, outer)
+		if inner != nil {
+			result = append(result, inner)
+		}
+	}
+	return result
 }
